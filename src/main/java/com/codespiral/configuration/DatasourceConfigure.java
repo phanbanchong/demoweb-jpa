fccc9a0c8307110ca4062106cefda6419152bcf8
Phanbanchong M. Suwan <mcodespiral@gmail.com>
2018-10-15 02:07:35 +0700

    Using JNDI Connection Pool

--- src/main/java/com/codespiral/configuration/DatasourceConfigure.java
+++ src/main/java/com/codespiral/configuration/DatasourceConfigure.java
@@ -4,25 +4,26 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 
 @Configuration
 public class DatasourceConfigure {
 
-	//@Value("${spring.datasource.jndi-name}")
-	//private String primaryJndiName;
+	@Value("${spring.datasource.jndi-name}")
+	private String primaryJndiName;
 	
-	//private JndiDataSourceLookup lookup = new JndiDataSourceLookup();
+	private JndiDataSourceLookup lookup = new JndiDataSourceLookup();
 
 	@Bean
 	@ConfigurationProperties(prefix = "spring.datasource")
 	public DataSource dataSourceProperties() throws SQLException {
-		return DataSourceBuilder.create().driverClassName("org.postgresql.Driver").build();
-		//DataSource dataSource = lookup.getDataSource(primaryJndiName);
-		//return dataSource;
+		//return DataSourceBuilder.create().driverClassName("org.postgresql.Driver").build();
+		DataSource dataSource = lookup.getDataSource(primaryJndiName);
+		return dataSource;
 	}
 
 }
